f964b69fabad69e7257c5f3cf93956a633bd603b
johannespischinger <johannes.pischinger@tum.de>
2022-01-06 11:27:39 +0100

    Branching

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -39,152 +39,63 @@ class CNNModel(nn.Module):
         return x
 
 
-class TrainOREvaluate(object):
-    """Helper class that will help launch class methods as commands
-    from a single script
-    """
+def train(self, model: nn.Model, epoch: float = 5, batchsize: int = 64, dataset: torch.nn.Dataset, learning_rate: float = 0.001,\
+          loss: torch.nn.Loss,\
+          optimizer: torch.optim):
 
-    def __init__(self):
-        """
-        Class to facilitate train and evaulation of model
+    trainloader = torch.utils.data.DataLoader(dataset, batchsize, shuffle=True)
 
-        Methods
-        -----
-        train(self):
-            Training model
+    criterion = loss
+    loss_hist = []
+    for e in range(epoch):
+        running_loss = 0
+        acc = []
+        for images, labels in trainloader:
+            optimizer.zero_grad()
 
-            Parameters
-            ____
-            None
+            images = images.unsqueeze(1)
 
-            Return
-            ___
-            None
+            output = model(images.float())
+            ps = torch.exp(output)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            acc.append(torch.mean(equals.type(torch.FloatTensor)))
 
+            loss = criterion(output, labels)
+            loss.backward()
 
-        evaluate: Evaluate model with testset
+            optimizer.step()
 
+            running_loss += loss.item()
 
+        else:
+            print(f"Training loss: {running_loss / len(trainloader)}")
+            print(f"Accuracy: {sum(acc) / len(acc)}")
 
-        """
-        parser = argparse.ArgumentParser(
-            description="Script for either training or evaluating",
-            usage="python main.py <command>",
-        )
-        parser.add_argument("command", help="Subcommand to run")
-        args = parser.parse_args(sys.argv[1:2])
-        if not hasattr(self, args.command):
-            print("Unrecognized command")
+        loss_hist.append(running_loss)
 
-            parser.print_help()
-            exit(1)
-        # use dispatch pattern to invoke method with same name
-        getattr(self, args.command)()
+    checkpoint = {
+        "input_size": (64, 1, 28, 28),
+        "output_size": 10,
+        "optimizer_state_dicts": optimizer.state_dict(),
+        "state_dict": model.state_dict(),
+    }
 
-    def train(self):
-        print("Training day and night")
-        parser = argparse.ArgumentParser(description="Training arguments")
-        parser.add_argument("--lr", default=0.1)
-        # add any additional argument that you want
-        args = parser.parse_args(sys.argv[2:])
-        print(args)
+    modelstring = str(f"b_{batchsize}_e_{epoch}_lr_{lr}")
+    torch.save(
+        checkpoint,
+        f"/Users/johannespischinger/Documents/Uni/Master/Erasmus/Courses/MLOps/cookiecutter-ds-project/\
+        models/trained_model_{modelstring}.pt",
+    )
 
-        model = CNNModel()
-        train_set = torch.load(
-            "/Users/johannespischinger/Documents/Uni/Master/Erasmus/Courses/MLOps/cookiecutter-ds-project/\
-            data/processed/train"
-        )
-        epoch = 5
-        batchsize = 64
-        trainloader = torch.utils.data.DataLoader(train_set, batchsize, shuffle=True)
-        lr = 0.003
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        criterion = torch.nn.NLLLoss()
-        loss_hist = []
-        for e in range(epoch):
-            running_loss = 0
-            acc = []
-            for images, labels in trainloader:
-                optimizer.zero_grad()
-
-                images = images.unsqueeze(1)
-
-                output = model(images.float())
-                ps = torch.exp(output)
-                top_p, top_class = ps.topk(1, dim=1)
-                equals = top_class == labels.view(*top_class.shape)
-                acc.append(torch.mean(equals.type(torch.FloatTensor)))
-
-                loss = criterion(output, labels)
-                loss.backward()
-
-                optimizer.step()
-
-                running_loss += loss.item()
-
-            else:
-                print(f"Training loss: {running_loss / len(trainloader)}")
-                print(f"Accuracy: {sum(acc) / len(acc)}")
-
-            loss_hist.append(running_loss)
-
-        checkpoint = {
-            "input_size": (64, 1, 28, 28),
-            "output_size": 10,
-            "optimizer_state_dicts": optimizer.state_dict(),
-            "state_dict": model.state_dict(),
-        }
-
-        modelstring = str(f"b_{batchsize}_e_{epoch}_lr_{lr}")
-        torch.save(
-            checkpoint,
-            f"/Users/johannespischinger/Documents/Uni/Master/Erasmus/Courses/MLOps/cookiecutter-ds-project/\
-            models/trained_model_{modelstring}.pt",
-        )
-
-        plt.plot(loss_hist)
-        plt.savefig(
-            f"/Users/johannespischinger/Documents/Uni/Master/Erasmus/Courses/MLOps/cookiecutter-ds-project/\
-            reports/figures/training_curve_{modelstring}.png"
-        )
-
-    def evaluate(self):
-        print("Evaluating until hitting the ceiling")
-        parser = argparse.ArgumentParser(description="Training arguments")
-        parser.add_argument("load_model_from", default="")
-        # add any additional argument that you want
-        args = parser.parse_args(sys.argv[2:])
-        print(args)
-
-        checkpoint = torch.load(args.load_model_from)
-        model = CNNModel()
-        model.load_state_dict(checkpoint["state_dict"])
-        # !!!! Important to turn off dropout
-        model.eval()
-        test_set = torch.load(
-            "/Users/johannespischinger/Documents/Uni/Master/Erasmus/Courses/MLOps/cookiecutter-ds-project/\
-            data/processed/test"
-        )
-        batchsize = 64
-        epoch = 5
-        testloader = torch.utils.data.DataLoader(
-            test_set, batch_size=batchsize, shuffle=True
-        )
-
-        #  Important to forbid gradient calculation
-        with torch.no_grad():
-            running_loss = 0
-            test_acc = []
-            for images, labels in testloader:
-                images = images.unsqueeze(1)
-                ps = torch.exp(model(images.float()))
-                top_p, top_class = ps.topk(1, dim=1)
-                equals = top_class == labels.view(*top_class.shape)
-                test_acc.append(torch.mean(equals.type(torch.FloatTensor)))
-
-            else:
-                print(f"Accuracy: {sum(test_acc) / len(test_acc)}")
-
+    plt.plot(loss_hist)
+    plt.savefig(
+        f"/Users/johannespischinger/Documents/Uni/Master/Erasmus/Courses/MLOps/cookiecutter-ds-project/\
+        reports/figures/training_curve_{modelstring}.png"
+    )
 
 if __name__ == "__main__":
-    TrainOREvaluate()
+    model = CNNModel()
+    dataset_path = 'data/processed/train'
+    train(model,epoch = 5, batchsize= 64, dataset_path, learning_rate= 0.001, loss = torch.nn.NLLLoss(), optimizer= torch.optim.Adam(model.parameters(),learning_rate = 0.001))
+
