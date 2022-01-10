7e2853aafecf31f9a4e70eea17282d9d5c0e0e5a
johannespischinger <johannes.pischinger@tum.de>
2022-01-10 17:18:20 +0100

    Updating Path

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 from tests import _PATH_DATA
 
-@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/data/processed), reason='Data files not found')
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/data/processed'), reason='Data files not found')
 def test_load():
     test = torch.load(f'{_PATH_DATA}/data/processed/test.pt')
     train = torch.load(f'{_PATH_DATA}/data/processed/train.pt')
